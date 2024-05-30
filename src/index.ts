00004c1a95907936cebad18eb633d01d861a5d43
Hossam Hamza <hossamhamza.hm@gmail.com>
2024-05-30 16:03:15 +0300

    feat/ initialize basic express server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3030
+
+
+
+
+
+app.get("*", (req: express.Request, res: express.Response) => {
+    res.send("Server is up and running");
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
